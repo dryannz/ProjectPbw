a77dcc2d3677d870c19a00cef2e5c0f03c0367c4
Adriyan <adriyanfrx@gmail.com>
2026-06-25 13:33:23 +0700

    fiks

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -146,18 +146,6 @@
                     </div>
                 </div>
 
-                <div class="form-row">
-                    <div class="checkbox-wrapper" id="rememberMe">
-                        <div class="checkbox">
-                            <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="#1c1c1e" stroke-width="3">
-                                <polyline points="20 6 9 17 4 12" />
-                            </svg>
-                        </div>
-                        <span class="checkbox-label">Remember me</span>
-                    </div>
-                    <a href="#" class="forgot-link">Forgot password?</a>
-                </div>
-
                 <button type="submit" class="submit-btn">Sign In</button>
             </form>
         </div>
